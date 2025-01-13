325ba3e59bdffbcf5fa6405b79d6a09b94ae0127
Noons-ledev <noah.briet@icloud.com>
2025-01-13 15:55:42 +0100

    fix

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str.split()
-str = f"{str[5]} {str[6]} with {str[0]}"
+str = str[39:-63] + str[106:-17] + str[:6]
 print(str)
