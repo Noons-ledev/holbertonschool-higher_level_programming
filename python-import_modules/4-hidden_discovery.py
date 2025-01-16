6cdc55094713870cbbb809975098a2f95885e5eb
Noons-ledev <noah.briet@icloud.com>
2025-01-16 09:27:31 +0100

    fix t4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import inspect
     import hidden_4
     membres = inspect.getmembers(hidden_4, inspect.isfunction)
-    noms_fonctions = sorted([nom for nom, _ in membres])
-    for nom in noms_fonctions:
+    function_names = sorted([nom for nom, _ in membres])
+    for nom in function_names:
         if nom[0] != '_':
             print(nom)
