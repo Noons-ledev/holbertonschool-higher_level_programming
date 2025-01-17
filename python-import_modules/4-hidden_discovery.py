5e704400c1d384a9108ad056f9eb36d19b051a41
Noons-ledev <noah.briet@icloud.com>
2025-01-17 09:09:42 +0100

    fix variables t4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,8 +2,8 @@
 if __name__ == "__main__":
     import inspect
     import hidden_4
-    membres = inspect.getmembers(hidden_4, inspect.isfunction)
-    function_names = sorted([nom for nom, _ in membres])
-    for nom in function_names:
+    members = inspect.getmembers(hidden_4, inspect.isfunction)
+    function_names = sorted([name for name, _ in members])
+    for name in function_names:
         if nom[0] != '_':
             print(nom)
