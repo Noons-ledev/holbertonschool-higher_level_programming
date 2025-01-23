0c22d889ec034728f67ad93973980a3b5b7bb0d7
Noons-ledev <noah.briet@icloud.com>
2025-01-23 11:42:57 +0100

    bonjour t1 finie

--- python-test_driven_development/2-main.py
+++ python-test_driven_development/2-main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
-matrix = []
-print(matrix_divided([[1, 2, 3],["hello", 2, 2]], 5))
-print(matrix)
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6]
+]
+print(matrix_divided([[1, 2, 3],[4, 5, 6]], float('inf')))
+
