8a7714ce4ea14c94dc86c126618ac80110bd30d8
Noons-ledev <noah.briet@icloud.com>
2025-01-23 11:58:38 +0100

    fixing tests, pycode and documentation on module

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+"""
+This module helps printing
+both fist and last name passed as argument
+to the function
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """
+    Checks first for string only as arguments
+    if not, raises errors
+    then performs the print
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
