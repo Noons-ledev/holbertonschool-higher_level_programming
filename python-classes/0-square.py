c8215cc5f151b00e235a35c38119c26f96ebc174
Noons-ledev <noah.briet@icloud.com>
2025-01-27 09:13:53 +0100

    fixing t0 t1

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+"""
+This module defines a class
+"""
+
+
 class Square:
     pass
+
+
+"""
+nothing happens here
+"""
--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""
+This module defines a class
+"""
+
+
 class Square:
 
     def __init__(self, size):
         self.__size = size
+
+
+"""
+This method initializes the square with
+a private instance attribute (size)
+"""
