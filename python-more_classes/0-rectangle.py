be4bf93095915c3b100626d1a91354deeba18cbe
Noons-ledev <noah.briet@icloud.com>
2025-01-27 11:56:15 +0100

    t0

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module is used to define the rectangle class and
+attributes and methods we'll find with it
+"""
+class Rectangle:
+    """
+    Here we'll implement some methods and attributes
+    """
+    pass
