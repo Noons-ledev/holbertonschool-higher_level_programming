186f070fc4f22ff17774a35cf82ad73934907985
Noons-ledev <noah.briet@icloud.com>
2025-01-27 10:41:50 +0100

    fixing spaces for tuple[1]

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -67,7 +67,7 @@ class Square:
         """
         if not isinstance(value, tuple) or\
             not all(isinstance(element, int) for element in value)\
-                or not all(element > 0 for element in value):
+                or not all(element >= 0 for element in value):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
@@ -80,6 +80,8 @@ class Square:
             print()
             return
         else:
+            for x in range(self.__position[1]):
+                print()
             for i in range(self.__size):
                 print(" " * self.__position[0], end="")
                 print("#" * self.__size)
