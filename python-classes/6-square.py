003996bcd02075d92a0e3768fb8dae8c5c847a3b
Noons-ledev <noah.briet@icloud.com>
2025-01-27 11:25:56 +0100

    private attribute

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -22,12 +22,12 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.size = size
         if not isinstance(position, tuple) or\
             not all(isinstance(value, int) for value in position)\
                 or not all(value >= 0 for value in position):
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = position
+        self.position = position
 
     def area(self):
         """
