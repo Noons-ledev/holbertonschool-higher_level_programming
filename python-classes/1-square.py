0810a6a31bbaaa4a0ada2e87cafb6646eefd2817
Noons-ledev <noah.briet@icloud.com>
2025-01-27 09:20:05 +0100

    adding doc to the class

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -6,6 +6,11 @@ This module defines a class
 
 class Square:
 
+    """
+    Here is my Square class
+    we'll add attributes here later
+    """
+
     def __init__(self, size):
         self.__size = size
 
