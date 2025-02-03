c661fd4ce34aaf04ef0d3cdf3d41753237a722c4
Noons-ledev <noah.briet@icloud.com>
2025-02-03 14:50:13 +0100

    bonjour t6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module implements the basegeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    This class is empty for now
+    """
+    def __init__(self):
+        """
+        Does nothing for now
+        """
+        pass
+
+    def area(self):
+        """
+        Raises an error for now due to lack of implementation
+        """
+        raise Exception("area() is not implemented")
