9534d38d3c3b4357568ae902ae4a43f9e54493cf
Noons-ledev <noah.briet@icloud.com>
2025-02-03 09:37:52 +0100

    t0

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module implements the lookup function
+"""
+def lookup(obj):
+    """
+    This function will return
+    method and attributes from a class
+    """
+    return (dir(obj))
