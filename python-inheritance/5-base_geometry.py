e22c5506a0c6e7588304b1ab073e88f3abb633a5
Noons-ledev <noah.briet@icloud.com>
2025-02-03 14:47:22 +0100

    bonjour t5

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module implements the basegeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    This class is empty for now
+    """
+    def __init__(self):
+        """
+        Does nothing for now
+        """
+        pass
