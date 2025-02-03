3b82fbba98fc1dee88b27ff8f1b2efc09356a7ce
Noons-ledev <noah.briet@icloud.com>
2025-02-03 15:00:49 +0100

    t7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This module implements the basegeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    This class is empty for now
+    """
+    def __init__(self):
+        """
+        Does nothing for now
+        """
+        pass
+
+    def area(self):
+        """
+        Raises an error for now due to lack of implementation
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        This method will validate value if conditions are respected
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        elif value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
