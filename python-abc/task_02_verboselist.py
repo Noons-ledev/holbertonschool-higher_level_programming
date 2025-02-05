25dd2ffb22f91533a16d103ef1f3479919680822
Noons-ledev <noah.briet@icloud.com>
2025-02-05 15:48:58 +0100

    bonjour

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 Comment here
 """
@@ -10,18 +10,30 @@ class VerboseList(list):
     """
 
     def append(self, item):
+        """
+        Method doc here
+        """
         print(f"Added [{item}] to the list.")
         super().append(item)
 
     def extend(self, item):
+        """
+        method doc here
+        """
         print(f"Extended the list with [{len(item)}] items.")
         super().extend(item)
 
     def remove(self, item):
+        """
+        method doc here
+        """
         print(f"Removed [{item}] from the list.")
         super().remove(item)
 
     def pop(self, item=-1):
+        """
+        method doc here
+        """
         if self[item] is None:
             raise IndexError("Index does not exist")
         print(f"Popped [{self[item]}] from the list.")
