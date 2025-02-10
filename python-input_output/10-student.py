a9871c81ae24d4a5e46cd6c4d3cab1cb77f33271
Noons-ledev <noah.briet@icloud.com>
2025-02-10 18:27:59 +0100

    Hasattr, getattr

---
+++ python-input_output/10-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module documentation
+"""
+
+
+class Student:
+    """
+    Class documentation
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes an instance of a class
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Doc here
+        """
+        if attrs is None:
+            return vars(self)
+        else:
+            mydict = {}
+            for i in attrs:
+                if hasattr(self, i):
+                    mydict[i] = getattr(self, i)
+        return mydict
