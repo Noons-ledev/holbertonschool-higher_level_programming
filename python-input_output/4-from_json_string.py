b66934924eb071622d1aed83e043de4d5ef6aa85
Noons-ledev <noah.briet@icloud.com>
2025-02-10 10:30:48 +0100

    deserializing

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module doc
+"""
+import json
+"""
+yeah
+"""
+
+
+def from_json_string(my_str):
+    """
+    Function doc
+    """
+    it = json.load(my_str)
+    return it
