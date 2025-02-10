8286a6e836f0a8b1e3baa1317bb7f1b5e03a8ffe
Noons-ledev <noah.briet@icloud.com>
2025-02-10 10:26:46 +0100

    JSON representation of an object with the json module

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module doc
+"""
+import json
+"""
+to method json.dump our object
+"""
+
+
+def to_json_string(my_obj):
+    """
+    Function doc
+    """
+    it = json.dumps(my_obj)
+    return it
