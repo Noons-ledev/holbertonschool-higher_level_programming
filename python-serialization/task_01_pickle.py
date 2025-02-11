c02a8c92a3586009cae09003578115bb3eec97fb
Noons-ledev <noah.briet@icloud.com>
2025-02-11 11:14:49 +0100

    using pickle with bytes

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Module documentation
+"""
+import pickle
+"""
+serializing with that module this time
+"""
+
+
+class CustomObject:
+    """
+    Class documentation
+    """
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = bool(is_student)
+
+    def display(self):
+        print("Name: {}".format(self.name))
+        print("Age: {}".format(self.age))
+        print("Is Student: {}".format(self.is_student))
+
+    def serialize(self, filename):
+        """
+        method doc
+        """
+        with open(filename, 'wb') as file:
+            tosavedata = pickle.dumps(self)
+            file.write(tosavedata)
+
+    @classmethod
+    def deserialize(cls, filename):
+        with open(filename, 'rb') as file:
+            return (pickle.loads(file.read()))
