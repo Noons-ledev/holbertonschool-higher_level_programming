9d69d82706f413a63e8ba2424fd66d702e7bd6f7
Noons-ledev <noah.briet@icloud.com>
2025-03-06 23:14:57 +0100

    Adding doc

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+Creates the table in our database
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -6,6 +9,9 @@ Base = declarative_base()
 
 
 class State(Base):
+    """
+    This is the state table we wanna create
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
