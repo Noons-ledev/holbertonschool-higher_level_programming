a7b29e4062956267db168bceec0be582dab9181b
Noons-ledev <noah.briet@icloud.com>
2025-03-06 21:28:54 +0100

    Listing cities on a new database

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This script list cities from a database
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost",
+                           user=sys.argv[1],
+                           passwd=sys.argv[2],
+                           db=sys.argv[3],
+                           port=3306)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM cities ORDER BY id ASC")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    conn.close()
