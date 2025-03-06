71394bf73afe685c210d0fdbb166d27cb65bc2b4
Noons-ledev <noah.briet@icloud.com>
2025-03-06 11:16:29 +0100

    Fixing pycode

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import MySQLdb
 import sys
-db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2],
+                     db=sys.argv[3], port=3306)
 cursor = db.cursor()
 
 cursor.execute("SELECT * FROM states;")
