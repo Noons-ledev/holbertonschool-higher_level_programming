f7bb076816e839be7df3e7a590a8970ea895bdf9
Noons-ledev <noah.briet@icloud.com>
2025-03-06 14:50:49 +0100

    filter states

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+DOCSTRING MANDATORY
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    db = MySQLdb.connect(host='localhost', user=mysql_username,
+                         passwd=mysql_username,
+                         db=database_name, port=3306)
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'"
+                   "ORDER BY id ASC;")
+    tables = cursor.fetchall()
+    for table in tables:
+        print(table)
+
+    cursor.close()
+    db.close()
