bf68213b62f2216fb676cfcfa0188902e7089971
Noons-ledev <noah.briet@icloud.com>
2025-03-06 15:29:24 +0100

    Fixing? "

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 """
-DOCSTRING MANDATORY
+DOCSTRING MANDATORY TO RETRIEVE THINGS
 """
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
 
-    db = MySQLdb.connect(host='localhost', user=mysql_username,
-                         passwd=mysql_username,
-                         db=database_name, port=3306)
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
     cur = db.cursor()
 
     cur.execute("SELECT * FROM states WHERE name LIKE BINARY"
