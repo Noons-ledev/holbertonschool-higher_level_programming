36c6dbbdbc5934a4b9adf4795bdec72c24a23135
Noons-ledev <noah.briet@icloud.com>
2025-03-06 11:23:03 +0100

    adding order by and cursor.close()

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,14 +10,14 @@ if __name__ == "__main__":
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
 
-db = MySQLdb.connect(host='localhost', user=mysql_username, passwd=mysql_username,
+    db = MySQLdb.connect(host='localhost', user=mysql_username, passwd=mysql_username,
                      db=database_name, port=3306)
-cursor = db.cursor()
+    cursor = db.cursor()
 
-cursor.execute("SELECT * FROM states ORDER BY id ASC;")
-tables = cursor.fetchall()
-for table in tables:
-    print(table)
+    cursor.execute("SELECT * FROM states ORDER BY id ASC;")
+    tables = cursor.fetchall()
+    for table in tables:
+        print(table)
 
-cursor.close()
-db.close()
+    cursor.close()
+    db.close()
