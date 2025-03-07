4c5701afc1ae8717f473c691f8f714c7f14686f2
Noons-ledev <noah.briet@icloud.com>
2025-03-07 07:46:18 +0100

    Using SQLAlchemy, session maker to interact on my database MySQL

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Using SQLAlchemy to retrieve a liste of states from a db table
+"""
+
+if __name__ == '__main__':
+
+    from sqlalchemy import create_engine, text
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine('mysql+mysqldb://root:root@localhost/hbtn_0e_6_usa')
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.execute(text("SELECT * from states"))
+    for row in result:
+        print(row)
