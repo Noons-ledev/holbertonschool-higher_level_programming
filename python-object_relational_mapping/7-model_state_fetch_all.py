4364e335534cf2d3efc9f26f938cd66bb9d63494
Noons-ledev <noah.briet@icloud.com>
2025-03-07 07:58:14 +0100

    Importing Base & State

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,12 +7,13 @@ if __name__ == '__main__':
 
     from sqlalchemy import create_engine, text
     from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
 
     engine = create_engine('mysql+mysqldb://root:root@localhost/hbtn_0e_6_usa')
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    result = session.execute(text("SELECT * from states"))
+    result = session.execute(text("SELECT * from states ORDER BY id"))
     for id, name in result:
         print("{}: {}".format(id, name))
