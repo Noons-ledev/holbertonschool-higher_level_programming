be3de7009ca6842cb3704836d4c473572bcf6235
Noons-ledev <noah.briet@icloud.com>
2025-03-07 08:16:20 +0100

    fetch the first using first method in my session.query

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Using SQLAlchemy to retrieve a liste of states from a db table
+"""
+
+if __name__ == '__main__':
+
+    from sqlalchemy import create_engine, text
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+    import sys
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]
+        ),
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).order_by(State.id).first()
+    print("{}: {}".format(state.id, state.name))
