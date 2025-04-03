165f639b509f87770961d00f4ce0f06f4317eabc
Noons-ledev <noah.briet@icloud.com>
2025-04-03 10:22:12 +0200

    adding a class on event

---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,6 @@
+const header = document.querySelector('header');
+const button = document.getElementById('red_header');
+
+button.addEventListener('click', function() {
+    header.classList.add('red');
+});
